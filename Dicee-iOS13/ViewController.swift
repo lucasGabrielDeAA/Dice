5d188a4cf8364b27439cc3dbed9a0eb86c221fc4
lucasGabrielDeAA <lg.technus@gmail.com>
2023-01-15 13:25:08 -0300

    chore: random values used to roll the dices

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImaveViewTwo: UIImageView!
     
+    let diceImages: [UIImage] = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        // Random value generated using Int.random within array boundaries
+        diceImageViewOne.image = diceImages[Int.random(in: 0...diceImages.count - 1)]
+        // Random value generated using randomElement's method
+        diceImaveViewTwo.image = diceImages.randomElement()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageViewOne.image = UIImage(named: "DiceSix")
-        diceImaveViewTwo.image = UIImage(named: "DiceTwo")
     }
 
 
